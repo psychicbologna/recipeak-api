93a6d3b51c8f23d5a49b81adf8e54bb6c93096c5
psychicbologna <alexander.fukui@gmail.com>
2019-10-14 10:26:48 -0500

    constructing migrations for recipeak; running into issues with db

--- migrations/001.do.create_recipeak_recipes.sql
+++ migrations/001.do.create_recipeak_recipes.sql
@@ -1,6 +1,11 @@
+--Add author when user table created;
+
 CREATE TABLE recipes (
   id SERIAL PRIMARY KEY,
   name TEXT NOT NULL,
+  author TEXT NOT NULL,
   ingredients TEXT[][] NOT NULL,
-  instructions TEXT NOT NULL
+  instructions TEXT NOT NULL,
+  prep_time TIME(HH:MM),
+  yield: INTEGER,
 )
---
+++ test/test-helpers.js
@@ -0,0 +1,12 @@
+function makeRecipesArray() {
+  return [
+    id: 1,
+    name: "Cast Iron Pizza with Fennel and Sausage"
+    ingredients:
+    instructions:
+  ]
+}
+
+module.exports = { 
+  makeRecipesArray
+};
