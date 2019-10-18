7493e44426a589a1a6d5d1435c166d53c65cbddd
Alexander Fukui <alexander.fukui@gmail.com>
2019-10-18 15:18:51 -0500

    refined serialize to ref units, promises!!

--- src/recipes/recipes-service.js
+++ src/recipes/recipes-service.js
@@ -6,8 +6,17 @@ const RecipesService = {
   //TODO: Make auth only
   getAllRecipes(db) {
     return db
-      .from('recipes')
-      .select('*');
+      .from('recipes AS rec')
+      .select(
+        'rec.id',
+        'rec.name',
+        'rec.author',
+        'rec.instructions',
+        'rec.prep_time',
+        'rec.servings',
+        'rec.date_created',
+        'rec.date_modified',
+      );
   },
   //TODO: add this to landing page.
   getAllPublicRecipes(db) {
@@ -30,22 +39,40 @@ const RecipesService = {
       .where({ id })
       .first();
   },
+
   getIngredientsForRecipe(db, recipe_id) {
     return db
       .from('ingredients AS ing')
       .select(
         'ing.id',
         'ing.amt',
-        'ing.ingredient'
+        'ing.unit_set',
+        'ing.unit_data',
+        'ing.ingredient',
       )
-      .where('ing.recipe_id', recipe_id);
+      .where('recipe_id', recipe_id);
   },
+
+  getAllUnitSets(db){
+    return db
+      .from('units')
+      .select('*');
+  },
+
+  getUnitSetData(db, unit_set) {
+    return db
+      .from('units')
+      .select('unit_data')
+      .where('unit_set', unit_set);
+  },
+
   deleteRecipe(db, id) {
     return db
       .from('recipes')
       .where({ id })
       .delete();
   },
+
   updateRecipe(db, id, newRecipeFields) {
     return db
       .from('recipes')
@@ -59,21 +86,31 @@ const RecipesService = {
       name: xss(recipe.name),
       author: xss(recipe.author),
       instructions: xss(recipe.instructions),
-      prep_time: setInterval(recipe.prep_time),
+      prep_time: recipe.prep_time,
       servings: Number(recipe.servings),
       date_created: new Date(recipe.date_created),
       date_modified: new Date(recipe.date_modified) || null
     };
   },
 
-  serializeRecipeIngredient(ingredient) {
-    return {
+  serializeRecipeIngredient(db, ingredient) {
+    const returndata = {
       id: ingredient.id,
       recipe_id: ingredient.recipe_id,
       amount: ingredient.amt,
+      unit_set: ingredient.unit_set,
       ingredientText: xss(ingredient.ingredient),
-
     };
+    if (!ingredient.unit_set) {
+      returndata.unit_data = ingredient.unit_data;
+      return returndata;
+    } else {
+      return this.getUnitSetData(db, ingredient.unit_set)
+        .then(ud => {
+          returndata.unit_data = ud[0].unit_data;
+          return returndata;
+        });
+    }
   }
 };
 
