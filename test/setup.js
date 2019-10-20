9000634aa53e271d0f22a70181b376b7d90a6454
Alexander Fukui <alexander.fukui@gmail.com>
2019-10-20 15:39:52 -0500

    jwt auth

--- test/setup.js
+++ test/setup.js
@@ -1,10 +1,9 @@
 process.env.TZ = 'UTC';
 process.env.NODE_ENV = 'test';
-
-require('dotenv').config();
-
+process.env.JWT_SECRET = 'test-jwt-secret';
 process.env.TEST_DB_URL = process.env.TEST_DB_URL || 'postgresql://recipeak@localhost/recipeak-api-test';
 
+require('dotenv').config();
 const { expect } = require('chai');
 const supertest = require('supertest');
 
