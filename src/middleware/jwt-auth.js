2b51e0157fb3fcf4a1fe8b68ae462933aa579dc2
Alexander Fukui <alexander.fukui@gmail.com>
2019-10-22 02:11:45 -0500

    tab

--- src/middleware/jwt-auth.js
+++ src/middleware/jwt-auth.js
@@ -7,7 +7,7 @@ function requireAuth(req, res, next) {
   if (!authToken.toLowerCase().startsWith('bearer')) {
     return res.status(401).json({ error: 'Missing bearer token' });
   } else {
-    bearerToken = authToken.slice(7, authToken.length)
+    bearerToken = authToken.slice(7, authToken.length);
   }
 
   try {
