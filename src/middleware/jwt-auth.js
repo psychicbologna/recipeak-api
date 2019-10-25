8eea67aa19d744e94d65e1f4f4561e18c7bdeadf
Alexander Fukui <alexander.fukui@gmail.com>
2019-10-24 23:02:51 -0500

    attempt to fix stuff

--- src/middleware/jwt-auth.js
+++ src/middleware/jwt-auth.js
@@ -4,8 +4,7 @@ function requireAuth(req, res, next) {
   const authToken = req.get('Authorization') || '';
 
   let bearerToken;
-
-  if (!authToken.toLowerCase().startsWith('bearer')) {
+  if (!authToken.toLowerCase().startsWith('bearer ')) {
     return res.status(401).json({ error: 'Missing bearer token' });
   } else {
     bearerToken = authToken.slice(7, authToken.length);
@@ -14,7 +13,7 @@ function requireAuth(req, res, next) {
   try {
     const payload = AuthService.verifyJwt(bearerToken);
 
-    AuthService.getUserWithUsername(
+    AuthService.getUserWithUserName(
       req.app.get('db'),
       payload.sub,
     )
@@ -22,9 +21,8 @@ function requireAuth(req, res, next) {
         if (!user)
           return res.status(401).json({ error: 'Unauthorized request' });
 
-        delete user.password;
-        req.user = user;
-        next();
+        req.user = user
+        next()
       })
       .catch(err => {
         console.error(err);
