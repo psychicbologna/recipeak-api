aed52e040914357dce04e74acca1ec626ac2cba0
Alexander Fukui <alexander.fukui@gmail.com>
2019-12-27 12:48:59 -0600

    try again

--- src/config.js
+++ src/config.js
@@ -5,5 +5,5 @@ module.exports = {
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://recipeak@localhost/recipeak',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://recipeak@localhost/recipeak-test',
   JWT_SECRET: process.env.JWT_SECRET || 'cc70a9f0-016c-4a7b-8260-f9800dcc89a7',
-  JWT_EXPIRY: process.env.JWT_EXPIRY || '8h',
+  JWT_EXPIRY: process.env.JWT_EXPIRY || '8h'
 };
